e830089a442f7ebdfc8cd3bd47d39a87c503cada
lbarstow <lbarstow@macalester.edu>
2018-06-05 20:16:05 -0400

    save before making new branch

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
+var getPixels = require("get-pixels");
+var jimp = require('jimp');
+var quantize = require('quantize');
 
 var path = require('path');
+app.use(express.static(__dirname + '/images'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
@@ -9,3 +13,18 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+console.log("hello world")
+var array=[];
+var 
+// let pixels = (a) => {
+//
+// }
+
+getPixels("images/wood.jpg", function(err, pixels) {
+  console.log("HI");
+  if(err) {
+    console.log("Bad image path");
+    return;
+  }
+  console.log("got pixels", pixels.shape.slice());
+})
